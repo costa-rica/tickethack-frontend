6ea0cc41af030ebc5f2303898dfc695c5c7d13fc
Your name <Your email>
2024-09-25 12:39:04 +0200

    home

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
-console.log("chitos")
-document.querySelector('#buttonsearch').addEventListener('click', 
-    fetch("http://localhost:3000/trips/search/")
-    .then((Response => Response.json))
-    .then((trips) => console.log(trips))
+const arrival =  document.querySelector('#arrival')
+const  departure =  document.querySelector('#departure')
+const buttonsearch = document.querySelector('#buttonsearch')
+const calandar = document.querySelector('#calandar')
 
-  
+
+buttonsearch.addEventListener('click', 
+    function(){
+        fetch("http://localhost:3000/trips/search/", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                arrival: arrival.value,
+                departure: departure.value,
+                date: calandar.value,
+            }),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+               console.log('donées recus chitos')
+               console.log(data)
+            })
+        // document.querySelector('#departure')
+        // console.log(departure.value)
+        // console.log(arrival.value)
+        // console.log(calandar.value)
+        // console.log(buttonsearch.value)
+
+//     fetch("http://localhost:3000/trips/search/")
+//     .then((Response) => Response.json())
+//     .then((data) => console.log(data))
+// console.log('click')
+    }
   );
+// buttonsearch.addEventListener("click", function () {
+//     console.log("test"); // logs the className of my_element
+  
+//   });
